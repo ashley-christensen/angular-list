9bcd1642d3db74503490068c92e07daa3f270187
Ashley Christensen <96439033+ashley-christensen@users.noreply.github.com>
2022-11-09 13:19:08 -0800

    todo model

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../models/todo';
 
 @Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+	selector: 'app-todos',
+	templateUrl: './todos.component.html',
+	styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+	todos: Todo[]; //empty array of todos
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.todos = [
+			{
+				content: 'First todo',
+				completed: false,
+			},
+			{
+				content: 'Second todo',
+				completed: true,
+			},
+		];
+	}
 }
